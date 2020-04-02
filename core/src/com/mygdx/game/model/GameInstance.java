a6cfea698d1659609926bb44388026e73fc48745
eolweus <37080270+eolweus@users.noreply.github.com>
2020-04-02 09:53:30 +0200

    Update GameInstance to take in input

--- core/src/com/mygdx/game/model/GameInstance.java
+++ core/src/com/mygdx/game/model/GameInstance.java
@@ -1,14 +1,16 @@
 package com.mygdx.game.model;
 
 import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.controller.ViewController;
 
 public class GameInstance {
 
     private Array<Player> playerArray;
     private World world;
     private ObstacleFactory obstacleFactory;
+    private ViewController viewController;
 
-    public GameInstance(Array<Player> playerArray, World world, ObstacleFactory obstacleFactory) {
+    public GameInstance(Array<Player> playerArray, World world, ObstacleFactory obstacleFactory, ViewController viewController) {
         this.obstacleFactory = obstacleFactory;
         this.playerArray = playerArray;
         this.world = world;
@@ -18,6 +20,16 @@ public class GameInstance {
         for(int i=0; i < playerArray.size; i++) {
             playerArray.get(i).update(dt);
             world.update(dt);
+        }
+    }
+
+    // Todo: find out how we want input given
+    /*Idea: whatever class handles the actual input from the users puts a value into an array
+     signifying either null, jump or gravity change which is then sent into game instance
+    */
+    public void handleInput(Array<Integer> playerInput) {
+        for (int i = 0; i < playerArray.size; i++) {
+
         }
     }
 }
